107a8d607e338b8e456525d5af90d4262b63e2ce
root <root@localhost>
2024-04-02 11:25:03 +0600

    auto publish 1712035502905

---
+++ src/f.py
@@ -0,0 +1,15 @@
+def slowprint(text):
+    import time
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(0.1)  # Adjust the delay as needed
+
+slowprint("≈======================================Ω")
+slowprint("|~~~~~~~~~~~~mr.tariful Islam~~~~~~~~~~Ω")
+slowprint("|             assalamualaikum          Ω")
+slowprint("|~~~~~~~~~~~😊😊😊😊😊😊😊😊😊~~~~~~~~Ω")
+slowprint("|¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶Ω")
+slowprint("                                       Ω")  
+slowprint("                                       Ω") 
+slowprint("                                       Ω") 
+slowprint("=======================================Ω")
